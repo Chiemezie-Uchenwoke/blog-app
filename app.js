7b3bf67fa939c88431de9c38112e3bc8307863b4
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-06-12 17:58:11 +0100

    Create form logic and integrate backend

--- app.js
+++ app.js
@@ -13,10 +13,31 @@ app.use(bodyParser.urlencoded())
 // parse application/json
 app.use(bodyParser.json())
 
+const formData = {
+  title: "The effect of growth",
+  content: "Self-discovery is a process of self-reflection, exploration, and growth that can help you gain a clearer sense of purpose and direction in life. It can also lead to improved relationships, better decision-making, and enhanced well-being.",
+}
+
+const post = []
+const getPost = () => {
+  post.push(formData)
+  post.push(formData)
+  post.push(formData)
+  post.push(formData)
+  post.push(formData)
+  post.push(formData)
+  post.push(formData)
+  return post;
+}
+
+const allPost = getPost();
+console.log(allPost);
+
+
 app.get('/', (req, res) => {
-  res.render("index.ejs")
-})
+  res.render("index.ejs", {allBlogPost: allPost})
+});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
-})
+});
---
+++ public/js/form.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("newBlog");
+const createBlogBtn = document.getElementById("createBlog");
+const createBlogContainer = document.getElementById("blogContainer");
+const closeFormBtn = document.getElementById("closeForm");
+const blogWrapper = document.getElementById("blogWrapper");
+const blogOverlay = document.getElementById("blogOverlay");
+
+createBlogBtn.addEventListener("click", () => {
+    createBlogContainer.style.display = "none";
+    blogOverlay.style.display = "block";
+    blogWrapper.style.overflowY = "hidden";
+    form.style.display = "flex";
+});
+
+closeFormBtn.addEventListener("click", () => {
+    blogOverlay.style.display = "none";
+    blogWrapper.style.overflowY = "scroll";
+    createBlogContainer.style.display = "flex";
+    form.style.display = "none";
+});
